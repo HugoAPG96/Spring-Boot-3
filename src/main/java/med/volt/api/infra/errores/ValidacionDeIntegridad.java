255677066044854111c6571c2f887b89d19e34e4
hugo <alexanderhpg@gmail.com>
2024-07-13 07:55:38 -0500

    corrección de ValidacionDeIntegridad.java

--- src/main/java/med/volt/api/infra/errores/ValidacionDeIntegridad.java
+++ src/main/java/med/volt/api/infra/errores/ValidacionDeIntegridad.java
@@ -2,8 +2,6 @@ package med.volt.api.infra.errores;
 
 public class ValidacionDeIntegridad extends RuntimeException {
     public ValidacionDeIntegridad(String s) {
-        public ValidacionDeIntegridad(String s){
-            super(s);
-        }
+        super(s);
     }
 }
